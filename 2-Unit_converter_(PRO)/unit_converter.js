6430e89ada2c78e743cd897a70c683c3c25bd922
k7-github <100348682+k7-github@users.noreply.github.com>
2023-12-02 15:26:39 +0800

    Update unit_converter.js
    Variable declaration.

---
+++ 2-Unit_converter_(PRO)/unit_converter.js
@@ -0,0 +1,7 @@
+const num = document.getElementById("add-number");
+const feet = num * 3.281
+const meter = num * 0.305
+const gallon = num * 0.264
+const liter = num * 3.785
+const pound = num * 2.204
+const kilogram = num * 0.454
