46b81f7b037c51c85a4bedade97b54a6b68b75de
k7-github <100348682+k7-github@users.noreply.github.com>
2023-12-02 15:42:05 +0800

    Update unit_converter.js
    Changed the num variable from a const variable to a let variable.
    
    Added variables that used the document.getElementById() method.
    
    Tried to add an addEventListener() function to the convertBtn variable.

--- 2-Unit_converter_(PRO)/unit_converter.js
+++ 2-Unit_converter_(PRO)/unit_converter.js
@@ -1,7 +1,18 @@
-const num = document.getElementById("add-number");
+let num = 0
+const convertBtn = document.getElementById("convert")
+const lengthUnits = document.getElementById("meters-feet")
+const volumeUnits = document.getElementById("liters-gallons")
+const massUnits = document.getElementById("kilograms-pounds")
+
 const feet = num * 3.281
 const meter = num * 0.305
 const gallon = num * 0.264
 const liter = num * 3.785
 const pound = num * 2.204
 const kilogram = num * 0.454
+
+convertBtn.addEventListener(onclick, function() {
+    lengthUnits.textContent = "meters to feets; feet to meters"
+    volumeUnits.textContent = "liters to gallons; gallons to liters"
+    massUnits.textContent = "kilograms to pounds; pounds to kilograms"
+})
