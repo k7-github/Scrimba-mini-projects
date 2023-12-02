4617b4d45a2f0dddedfa295c013fe8874be8cc3d
k7-github <100348682+k7-github@users.noreply.github.com>
2023-12-02 16:07:01 +0800

    Update unit_converter.js
    Made changes to formulae variables.
    
    Made several changes to the convertBtn.addEventListener function.

--- 2-Unit_converter_(PRO)/unit_converter.js
+++ 2-Unit_converter_(PRO)/unit_converter.js
@@ -4,15 +4,16 @@ const lengthUnits = document.getElementById("meters-feet")
 const volumeUnits = document.getElementById("liters-gallons")
 const massUnits = document.getElementById("kilograms-pounds")
 
-const feet = num * 3.281
-const meter = num * 0.305
-const gallon = num * 0.264
-const liter = num * 3.785
-const pound = num * 2.204
-const kilogram = num * 0.454
+const feet = num.valueOf * 3.281
+const meter = num.valueOf * 0.305
+const gallon = num.valueOf * 0.264
+const liter = num.valueOf * 3.785
+const pound = num.valueOf * 2.204
+const kilogram = num.valueOf * 0.454
 
-convertBtn.addEventListener(onclick, function() {
-    lengthUnits.textContent = "meters to feets; feet to meters"
+convertBtn.addEventListener("click", function() {
+    num = document.getElementById("add-number")
+    lengthUnits.textContent = `${num} meters = ${feet} feet | ${num} feet = ${meter} meters.`
     volumeUnits.textContent = "liters to gallons; gallons to liters"
     massUnits.textContent = "kilograms to pounds; pounds to kilograms"
 })
