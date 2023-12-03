fa88f08683e07c936cfc40245d8a1ed791c8d802
k7-github <100348682+k7-github@users.noreply.github.com>
2023-12-03 16:12:15 +0800

    Update unit_converter.js
    Commeted out most of the equations and added "console.log("Is is working?")" and "console.log(num)" to test the button.

--- 2-Unit_converter_(PRO)/unit_converter.js
+++ 2-Unit_converter_(PRO)/unit_converter.js
@@ -16,12 +16,14 @@ const kilogramValue = document.getElementById("kilograms")
 // const kilogram = num.valueOf * 0.454
 
 convertBtn.addEventListener("click", function() {
-    num = document.getElementById("add-number")
-    initialValue.value = num
-    feetValue.value = num.value * 3.281
-    meterValue.value = num.value * 0.305
-    gallonValue.value = num.value * 0.264
-    literValue.value = num.value * 3.785
-    poundValue.value = num.value * 2.204
-    kilogramValue.value = num.value * 0.454
+    num.value = document.getElementById("add-number")
+    // initialValue.value = num
+    // feetValue.value = num.value * 3.281
+    // meterValue.value = num.value * 0.305
+    // gallonValue.value = num.value * 0.264
+    // literValue.value = num.value * 3.785
+    // poundValue.value = num.value * 2.204
+    // kilogramValue.value = num.value * 0.454
+    console.log("Is is working?")
+    console.log(num)
 })
