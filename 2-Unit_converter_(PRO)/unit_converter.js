3c71b1b695296f406a6f253f5a0bc61d35284bcc
k7-github <100348682+k7-github@users.noreply.github.com>
2023-12-03 16:00:31 +0800

    Update unit_converter.js
    Removed the following variables:
    - lengthUnits
    - volumeUnits
    - massUnits
    - feet
    - meter
    - gallon
    - liter
    - pound
    - kilogram
    
    Added the following new variables:
    - initialValue
    - feetValue
    - meterValue
    - gallonValue
    - literValue
    - poundValue
    - kilogramValue

--- 2-Unit_converter_(PRO)/unit_converter.js
+++ 2-Unit_converter_(PRO)/unit_converter.js
@@ -1,19 +1,27 @@
 let num = 0
 const convertBtn = document.getElementById("convert")
-const lengthUnits = document.getElementById("meters-feet")
-const volumeUnits = document.getElementById("liters-gallons")
-const massUnits = document.getElementById("kilograms-pounds")
+const initialValue = document.getElementsByClassName("initial-value")
+const feetValue = document.getElementById("feet")
+const meterValue = document.getElementById("meters")
+const gallonValue = document.getElementById("gallons")
+const literValue = document.getElementById("liters")
+const poundValue = document.getElementById("pounds")
+const kilogramValue = document.getElementById("kilograms")
 
-const feet = num.valueOf * 3.281
-const meter = num.valueOf * 0.305
-const gallon = num.valueOf * 0.264
-const liter = num.valueOf * 3.785
-const pound = num.valueOf * 2.204
-const kilogram = num.valueOf * 0.454
+// const feet = num.valueOf * 3.281
+// const meter = num.valueOf * 0.305
+// const gallon = num.valueOf * 0.264
+// const liter = num.valueOf * 3.785
+// const pound = num.valueOf * 2.204
+// const kilogram = num.valueOf * 0.454
 
 convertBtn.addEventListener("click", function() {
     num = document.getElementById("add-number")
-    lengthUnits.textContent = `${num} meters = ${feet} feet | ${num} feet = ${meter} meters.`
-    volumeUnits.textContent = "liters to gallons; gallons to liters"
-    massUnits.textContent = "kilograms to pounds; pounds to kilograms"
+    initialValue.value = num
+    feetValue.value = num.value * 3.281
+    meterValue.value = num.value * 0.305
+    gallonValue.value = num.value * 0.264
+    literValue.value = num.value * 3.785
+    poundValue.value = num.value * 2.204
+    kilogramValue.value = num.value * 0.454
 })
